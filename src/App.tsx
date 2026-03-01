28cf538e358c57ebf412101bd3b2643b94c9bdc3
Andrew Lesh <lite.nite628@gmail.com>
2026-02-28 21:01:19 -0500

    Update title.
    Customize CSS

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import { db } from './db.ts';
 import { NoteView } from './components/NoteView.tsx';
 import { NoteEdit } from './components/NoteEdit.tsx';
 
+import './index.css';
+import './App.css';
+
 export default function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
@@ -19,20 +22,20 @@ export default function App() {
   };
 
   return (
-    <main style={{ maxWidth: '600px', margin: '0 auto', padding: '20px', fontFamily: 'sans-serif' }}>
+    <main style={{ maxWidth: '1024px', margin: '0 auto', padding: '20px', fontFamily: 'sans-serif' }}>
       <h1>Day Draft</h1>
       
       {/* Creation UI */}
-      <div style={{ marginBottom: '30px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
+      <div id="main-form" style={{ marginBottom: '30px', display: 'flex', flexDirection: 'column', gap: '10px' }}>
         <input value={title} onChange={(e) => setTitle(e.target.value)} placeholder="Title..." />
         <textarea value={content} onChange={(e) => setContent(e.target.value)} placeholder="Content..." rows={3} />
         <button onClick={addNote}>Create Note</button>
       </div>
 
       {/* List UI */}
-      <section style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', gap: '.75rem' }}>
+      <section style={{ display: 'flex', justifyContent: 'space-between', flexWrap: 'wrap', alignItems: 'center', gap: '.75rem' }}>
         {notes?.map(note => (
-          <div key={note.id} style={{ border: '1px solid #ccc', marginBottom: '15px', padding: '15px', borderRadius: '8px' }}>
+          <div key={note.id} style={{ height: '100%', border: '1px solid #ccc', marginBottom: '15px', padding: '15px', borderRadius: '8px' }}>
             {editingId === note.id ? (
               <NoteEdit note={note} onDone={() => setEditingId(null)} />
             ) : (
